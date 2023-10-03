ef94546168bead060bd48bf37a8ba4eb024e1b60
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-03 13:21:40 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,31 @@
  *
  * Created by: Daki A.B
  * Created on: Sep 2023
- * This program ...
+ * This program plays Cookie Clicker on a MicroBit
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+
+// variables
+let numberOfCookiesPressed: number
+numberOfCookiesPressed = numberOfCookiesPressed + 1
+
+// on "A" button pressed
+
+numberOfCookiesPressed = 0
+
+input.onButtonPressed(Button.A, function () {
+
+  numberOfCookiesPressed = numberOfCookiesPressed + 1
+  basic.showNumber(numberOfCookiesPressed)
+
+})
+
+// on "B" button pressed
+
+input.onButtonPressed(Button.B, function () {
+
+  numberOfCookiesPressed = 0
+  basic.showNumber(numberOfCookiesPressed)
+
+})
