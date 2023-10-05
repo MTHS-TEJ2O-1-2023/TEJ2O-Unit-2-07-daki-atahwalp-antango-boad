fbcf1c66eef79b485c83bfedb682c44258cd923b
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-05 01:48:04 +0000

    cookie clicker .py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,24 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Daki A.B
+Created on: Oct 2023
+This module is a Cookie Clicker on MicroBit
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+number_of_cookies_pressed = 0
+
+# When button "A" and "B" is pressed
+
+while True:
+    if button_a.is_pressed():
+        number_of_cookies_pressed = number_of_cookies_pressed + 1
+        display.show(str(number_of_cookies_pressed))
+
+while True:
+    if button_b.is_pressed():
+        number_of_cookies_pressed = 0
+        display.show(str(number_of_cookies_pressed))
